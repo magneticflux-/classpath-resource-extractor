710c77b29fe44364200ac155f5e2611aa8f3244e
Mitchell Skaggs <skaggsm333@gmail.com>
2020-06-09 20:17:58 -0500

    Add test for nested zip

--- src/test/java/com/skaggsm/ClasspathUtilsTest.java
+++ src/test/java/com/skaggsm/ClasspathUtilsTest.java
@@ -3,11 +3,13 @@ package com.skaggsm;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by Mitchell Skaggs on 4/15/2020.
@@ -28,5 +30,20 @@ class ClasspathUtilsTest {
         assertTrue(Files.exists(path.resolve("test_dir")), "Extracted directory exists");
         assertTrue(Files.exists(path.resolve("test_dir").resolve("test_dir_file")), "Extracted file exists");
         assertEquals("contents of test_dir_file", Files.readAllLines(path.resolve("test_dir").resolve("test_dir_file")).get(0), "Extracted file has correct contents");
+    }
+
+    @Test
+    void given_nested_zip_when_visited_success() throws URISyntaxException, IOException {
+        URI zip1 = Objects.requireNonNull(this.getClass().getClassLoader().getResource("test_zip.zip")).toURI();
+        ClasspathUtils.visitUriAsPath(zip1, (ClasspathUtils.PathConsumer<IOException>) path -> assertTrue(Files.exists(path), "Outer zip exists"));
+
+        URI zip1File = new URI("jar", zip1.toString() + "!/test_zip_file", null);
+        ClasspathUtils.visitUriAsPath(zip1File, (ClasspathUtils.PathConsumer<IOException>) path -> assertTrue(Files.exists(path), "Outer zip file exists"));
+
+        URI zip2 = new URI("jar", zip1.toString() + "!/test_nested_zip.zip", null);
+        ClasspathUtils.visitUriAsPath(zip2, (ClasspathUtils.PathConsumer<IOException>) path -> assertTrue(Files.exists(path), "Inner zip exists"));
+
+        URI zip2File = new URI("jar", zip2.toString() + "!/test_nested_zip_file", null);
+        ClasspathUtils.visitUriAsPath(zip2File, (ClasspathUtils.PathConsumer<IOException>) path -> assertTrue(Files.exists(path), "Inner zip file exists"));
     }
 }
